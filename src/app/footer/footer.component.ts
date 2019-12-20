c4b3e66a1b9469b319d2a80eac157dd1c9f7de88
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2019-12-20 15:27:38 -0600

    added b&w logo to footer. need to rearrange now

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -12,4 +12,12 @@ export class FooterComponent implements OnInit {
   ngOnInit() {
   }
 
+  goToGoogleMaps() {
+    window.open("https://www.google.com/maps/dir//farmers+kitchen+desoto+mo/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x87d8e22b48c9cacf:0xab2e3288642b7d3b?sa=X&ved=2ahUKEwj3lvLLlLvmAhVIbs0KHXk-BVkQ9RcwDHoECBsQAw");
+  }
+
+  contactUs(){
+    window.open("tel:+6362092406")
+  }
+
 }
